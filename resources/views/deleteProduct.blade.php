6b1531be569e1a907ca0def7b216bf0ab1837aad
kevinwaker <waker.kevin@gmail.com>
2019-06-23 04:44:43 -0300

    modificado el formulario de deleteProduct para que solo confirme el proceso

--- resources/views/deleteProduct.blade.php
+++ resources/views/deleteProduct.blade.php
@@ -1,31 +1,15 @@
 @include('partials.header')
 
-<br>
 <div class="form-header">
   <h2 class="form-title">E<span>liminar</span> P<span>roducto</span> </h2>
 </div>
+<div class="contenedor">
+  <form class="formulariogrande" action="" method="post" enctype="multipart/form-data">
+   @csrf
+<div class="prueba1">Vas a borrar:</h2> <h4>{{old('name', $product->name)}}"></h4>
+ <br>
+     <button type="submit" name="button" class="btn btn-danger">Eliminar Producto</button>
+ </form>
 
-<div class="row">
-  <div class="col-12 col-md-6 col-lg-12">
-  <div class="card text-white bg-dark mb-3"
-  style="
-    text-align: center;
-    max-width: 35rem;
-    margin: auto;
-    padding-left: 0px;
-    padding-top: 10px;
-    ">
-    <a name="quienes">
-      <img src="/storage/{{$product->imagen}}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title" style="font-family: 'Orbitron', sans-serif;
-      font-weight: bold;">{{$product->name}}</h5>
-      <p class="card-text">{{$product->description}}</p>
-      <p class="card-text">Precio: ${{$product->price}}</p>
-      <p class="card-text">Stock: {{$product->stock}}</p>
-      @auth
-        <br>
-      <a href="/deleteProduct/{{$product->id}}" class="btn btn-danger">Eliminar</a>
-      @endauth
 
- </div>
+</div>
